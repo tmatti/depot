46e2990071b839742da00e3eb15a2f9731ce7b13
tmatti <tim.matti@careerbuilder.com>
2018-01-24 23:36:06 -0500

    added functional product controller tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,26 +4,29 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
     @update = {
-        title:       'Lorem Ipsum',
+        title: 'Lorem Ipsum',
         description: 'Wibbles are fun!',
-        image_url:   'lorem.jpg',
-        price:       19.95
+        image_url: 'lorem.jpg',
+        price: 19.95
     }
   end
 
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select '#main table tr', minimum: 3
+    assert_select '#main h1', 'Products'
   end
 
   test "should get new" do
     get new_product_url
     assert_response :success
+    assert_select '#main h1', 'New Product'
   end
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: @update }
+      post products_url, params: {product: @update}
     end
 
     assert_redirected_to product_url(Product.last)
@@ -37,10 +40,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get edit" do
     get edit_product_url(@product)
     assert_response :success
+    assert_select '#product_title' do |title|
+      assert_select '[value=?]', @product[:title]
+    end
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: @update }
+    patch product_url(@product), params: {product: @update}
     assert_redirected_to product_url(@product)
   end
 
