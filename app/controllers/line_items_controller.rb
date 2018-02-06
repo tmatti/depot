032fa7d37c0acdfba6d1409583bf2181111fdd5c
tmatti <tim.matti@careerbuilder.com>
2018-02-06 14:01:32 -0500

    sessions working and you can add products to your cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,9 @@
 class LineItemsController < ApplicationController
   include CurrentCart
+
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+
 
   # GET /line_items
   # GET /line_items.json
@@ -26,13 +28,18 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
-
+    puts '?????'
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
+    puts "testing"
     respond_to do |format|
+      puts "hmmm"
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        puts "hello!!!!!"
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
+        puts "ah fuck"
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
